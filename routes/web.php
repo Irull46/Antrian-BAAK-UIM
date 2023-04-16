3e5476035aad7b511ed32c477c554dd67b95b5b7
Irull46 <eenkanamz@gmail.com>
2023-04-16 15:08:17 +0700

    Membuat halaman role manajemen

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PanggilController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
 Route::get('/', HomeController::class);
 Route::resource('panggil', PanggilController::class);
+Route::resource('role', RoleController::class);
