86246c082c116406ff283fb4edbd485c980ac9a1
Irull46 <eenkanamz@gmail.com>
2023-04-18 15:48:31 +0700

    Update halaman cetak antrian

--- resources/views/cetak.blade.php
+++ resources/views/cetak.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row align-items-center justify-content-center">
+        <div class="row justify-content-center">
             <div class="col-md-4">
                 <div class="card p-5">
-                    <div class="text-center">
-                        <img src="{{ asset('images/BAAK Logo.png') }}" alt="BAAK Logo" height="70" class="mb-2">
-                        <div class="fs-3 fw-bold ">Cetak Antrian</div>
-                        <div class="fs-5 mb-3">Masukkan jumlah antrian yang akan dicetak</div>
-                    </div>
-        
                     <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{ asset('images/BAAK Logo.png') }}" alt="BAAK Logo" height="70" class="mb-2">
+                            <div class="fs-3 fw-bold ">Cetak Antrian</div>
+                            <div class="fs-5 mb-3">Masukkan jumlah antrian yang akan dicetak</div>
+                        </div>
+            
                         <form>
                             <div>
                                 <input type="text" class="form-control mb-3" id="cetakAntrian" placeholder="Input jumlah">
