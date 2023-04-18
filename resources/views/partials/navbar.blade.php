f24143589a3d0eaf5b423d6497512526cf3acff0
Irull46 <eenkanamz@gmail.com>
2023-04-18 20:57:51 +0700

    Update navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -31,13 +31,13 @@
           @guest
           @if (Route::has('login'))
               <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  <a class="nav-link" href="{{ route('login') }}">Login</a>
               </li>
           @endif
 
           @if (Route::has('register'))
               <li class="nav-item">
-                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                  <a class="nav-link" href="{{ route('register') }}">Daftar</a>
               </li>
           @endif
           @else
@@ -49,8 +49,7 @@
                   <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                       <a class="dropdown-item" href="{{ route('logout') }}"
                         onclick="event.preventDefault();
-                                      document.getElementById('logout-form').submit();">
-                          {{ __('Logout') }}
+                        document.getElementById('logout-form').submit();">Logout
                       </a>
 
                       <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
