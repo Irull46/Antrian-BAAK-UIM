7e111243c0c455d5f942fdd54098486aa5ad6925
Irull46 <eenkanamz@gmail.com>
2023-04-19 00:05:39 +0700

    Update halaman login, halaman email (verifikasi akun), dan halaman reset (setel ulang password)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,8 +14,8 @@
 
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-                            <div>
-                                <label for="email" class="col-md-4 col-form-label">Email</label>
+                            <div class="mb-2">
+                                <label for="email">Email</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -25,8 +25,8 @@
                                 @enderror
                             </div>
                                 
-                            <div class="mb-1">
-                                <label for="password" class="col-md-4 col-form-label">Password</label>
+                            <div class="mb-2">
+                                <label for="password">Password</label>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+            <div class="col-sm-8 col-md-7 col-lg-6 col-xl-5">
+                <div class="card p-sm-4 p-lg-5">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{ asset('images/BAAK Logo.png') }}" alt="BAAK Logo" height="70" class="mb-2">
+                            <div class="fs-3 fw-bold">Setel Ulang Password</div>
+                            <div class="fs-5 mb-4">Masukkan email dan password untuk menyetel ulang password</div>
+                        </div>
+                        
+                        <form method="POST" action="{{ route('password.update') }}">
+                            @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+                            <input type="hidden" name="token" value="{{ $token }}">
 
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
+                            <div class="mb-2">
+                                <label for="email">Alamat Email</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -25,38 +27,28 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            
+                            <div class="row mb-3">
+                                <div class="col-md-6 pe-md-1">
+                                    <label for="password">Password</label>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+        
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                
+                                <div class="col-md-6 ps-md-1">
+                                    <label for="password-confirm">Ulangi Password</label>
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                            <button type="submit" class="btn btn-success">Setel Ulang Password</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
