b8c4487277f5b6dd9192dd88930d0087e3a842f7
Irull46 <eenkanamz@gmail.com>
2023-04-18 10:02:40 +0700

    Update halaman role manajemen

--- resources/views/role.blade.php
+++ resources/views/role.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container-fluid">
+        <div class="fs-3 fw-bold">Role Manajemen</div>
+        <div class="fs-5">Mengatur Hak Akses Pengguna</div>
+
+        {{-- Tabel Role --}}
+    </div>
 @endsection
