10cc8135c00ea61975490c6472c11dfe4ee710c4
Irull46 <eenkanamz@gmail.com>
2023-04-27 15:23:57 +0700

    Menginstall laravel permission

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+        $user->assignRole('admin');
+        
+        $user = User::create([
+            'name' => 'Teller',
+            'email' => 'teller@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+        $user->assignRole('teller');
+        
+        $user = User::create([
+            'name' => 'Pengunjung',
+            'email' => 'pengunjung@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+        $user->assignRole('pengunjung');
+    }
+}
