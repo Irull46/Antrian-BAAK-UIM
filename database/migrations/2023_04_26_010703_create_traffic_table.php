f48bf4aed17b5743dd49a8fbb18b2ad0e1175bbf
Irull46 <eenkanamz@gmail.com>
2023-05-08 08:40:42 +0700

    Mengubah nama migration traffics ke nama asalnya yaitu tanpa tambahan huruf s

--- database/migrations/2023_04_26_010703_create_traffic_table.php
+++ database/migrations/2023_04_26_010703_create_traffic_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTrafficsTable extends Migration
+class CreateTrafficTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTrafficsTable extends Migration
      */
     public function up()
     {
-        Schema::create('traffics', function (Blueprint $table) {
+        Schema::create('traffic', function (Blueprint $table) {
             $table->id();
             $table->foreignId('antrian_id')->constrained('antrians')->onDelete('cascade');
             $table->dateTime('mulai_pelayanan');
